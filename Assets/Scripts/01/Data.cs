8047b0d5e3af9de3fb7e2cefb1244c78c5d6c3c3
Bill <70634443+BillStewartThomas@users.noreply.github.com>
2024-08-12 23:37:03 +0800

    测试项目

---
+++ Assets/Scripts/01/Data.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+public class Data : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("HelloWorld!!!");
+    }
+    
+    private void Update()
+    {
+    }
+}
